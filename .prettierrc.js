9e6fdc8a4e4e0d6a4872a04990188eee40286d14
Ömer Balyalı <git@omerbalyali.com>
2025-05-22 21:49:04 +0300

    chore(repo): add prettier-plugin-packagejson

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,6 @@
 module.exports = {
 	$schema: 'https://json.schemastore.org/prettierrc',
+	plugins: ['./common/autoinstallers/rush-prettier/node_modules/prettier-plugin-packagejson/lib/index.cjs'],
 	printWidth: 120,
 	tabWidth: 2,
 	useTabs: true,

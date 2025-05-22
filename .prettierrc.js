16f64c0cb67b2628c2319f0d859ac49f7b32a3e8
Ömer Balyali <omerbalyali@gmail.com>
2025-05-22 20:55:13 +0300

    [repo] Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	$schema: 'https://json.schemastore.org/prettierrc',
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	arrowParens: 'always',
+	bracketSpacing: true,
+}

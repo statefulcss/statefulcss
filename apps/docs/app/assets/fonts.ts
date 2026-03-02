8f9c3a59edbdc0da4890ede2253667df480d3883
Ömer Balyalı <git@omerbalyali.com>
2026-03-02 13:51:58 +0100

    chore: formatting
    Signed-off-by: Ömer Balyalı <git@omerbalyali.com>

--- apps/docs/app/assets/fonts.ts
+++ apps/docs/app/assets/fonts.ts
@@ -1,18 +1,18 @@
 import localFont from "next/font/local";
 
 export const Switzer = localFont({
-  src: [
-    {
-      path: "./fonts/Switzer-Variable.woff2",
-      weight: "100 900",
-      style: "normal",
-    },
-    {
-      path: "./fonts/Switzer-VariableItalic.woff2",
-      weight: "100 900",
-      style: "italic",
-    },
-  ],
-  display: "swap",
-  variable: "--font-switzer",
+	src: [
+		{
+			path: "./fonts/Switzer-Variable.woff2",
+			weight: "100 900",
+			style: "normal",
+		},
+		{
+			path: "./fonts/Switzer-VariableItalic.woff2",
+			weight: "100 900",
+			style: "italic",
+		},
+	],
+	display: "swap",
+	variable: "--font-switzer",
 });

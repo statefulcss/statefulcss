926d97599fa30820acd015f37effda3e337b8b9e
Ömer Balyalı <git@omerbalyali.com>
2026-03-02 13:25:25 +0100

    feat(docs): add simple layout and page
    Signed-off-by: Ömer Balyalı <git@omerbalyali.com>

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,11 +1,63 @@
+import { Switzer } from "@/app/assets/fonts";
+import "@/styles/index.css";
+import type { Metadata } from "next";
+import styles from "./layout.module.css";
+
+const siteURL = "https://statefulcss.com";
+const siteName = "Stateful CSS";
+const defaultDescription = "Stateful CSS";
+const defaultOgImage = `${siteURL}/og-default.png`;
+
+export const metadata: Metadata = {
+	metadataBase: new URL(siteURL),
+	title: {
+		default: siteName,
+		template: `%s — ${siteName}`,
+	},
+	description: defaultDescription,
+	openGraph: {
+		type: "website",
+		locale: "en_US",
+		url: siteURL,
+		title: {
+			default: siteName,
+			template: `%s — ${siteName}`,
+		},
+		description: defaultDescription,
+		images: [
+			{
+				url: defaultOgImage,
+				width: 1200,
+				height: 630,
+				alt: siteName,
+			},
+		],
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: {
+			default: siteName,
+			template: `%s — ${siteName}`,
+		},
+		description: defaultDescription,
+		creator: "@omerbalyali",
+		site: "@statefulcss",
+		images: [defaultOgImage],
+	},
+};
+
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
+		<html lang="en" data-theme="default" data-scheme="dark">
+			<body className={`${Switzer.className}`}>
+				<div className={`${styles.Layout} app-container`}>
+					<main className={styles.Page}>{children}</main>
+				</div>
+			</body>
 		</html>
 	);
 }
--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-	return <div></div>;
+	return <h1>Stateful CSS</h1>;
 }

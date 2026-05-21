2e2642355555334b81404c8979996e7e0ae460d9
Ömer Balyalı <git@omerbalyali.com>
2026-05-21 14:33:54 +0200

    chore(web): add web fonts
    Signed-off-by: Ömer Balyalı <git@omerbalyali.com>

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,7 +1,42 @@
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [sitemap()],
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "Switzer Variable",
+			cssVariable: "--font-switzer",
+			options: {
+				variants: [
+					{
+						src: ["./src/assets/fonts/Switzer-Variable.woff2"],
+						weight: "100 900",
+						style: "normal",
+					},
+					{
+						src: ["./src/assets/fonts/Switzer-Variable-Italic.woff2"],
+						weight: "100 900",
+						style: "italic",
+					},
+				],
+			},
+		},
+		{
+			provider: fontProviders.local(),
+			name: "JetBrains Mono",
+			cssVariable: "--font-jetbrains-mono",
+			options: {
+				variants: [
+					{
+						src: ["./src/assets/fonts/JetBrainsMono-Variable.woff2"],
+						weight: "100 900",
+						style: "normal",
+					},
+				],
+			},
+		},
+	],
 });
